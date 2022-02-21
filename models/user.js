4dde401090ace023bd89576e4b5f277bfc82d068
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-02-21 15:09:35 -0800

    adds static async method register to User class

--- models/user.js
+++ models/user.js
@@ -1,5 +1,12 @@
 "use strict";
 
+/** User class for message.ly */
+
+const { NotFoundError } = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+
 /** User of the site. */
 
 class User {
@@ -9,6 +16,21 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(
+          password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+          `INSERT INTO users (username,
+                              password,
+                              first_name,
+                              last_name, 
+                              phone)
+            VALUES 
+              ($1, $2, $3, $4, $5)
+            RETURNING username, password, first_name, last_name, phone`,
+            [username, hashedPassword, first_name, last_name, phone]);
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
